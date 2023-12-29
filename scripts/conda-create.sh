76a49a7f0ab959e279c53bb3942354389782ee63
neucompute <neucompute@phi>
2023-12-29 11:37:48 -0800

    Install ipykernel and set up the project kernel
    Do not install jupyter in the project environment.

--- scripts/conda-create.sh
+++ scripts/conda-create.sh
@@ -5,12 +5,13 @@ CONDA_PREFIX='envs/sam'
 conda create -y -p ${CONDA_PREFIX} python=3.11
 
 conda install -y -p ${CONDA_PREFIX} \
-    requests tqdm regex ftfy jupyter \
+    requests tqdm regex ftfy ipykernel \
     numpy scipy scikit-learn matplotlib pandas \
     pyarrow hdf5 pytables \
     pillow
 
-${CONDA_PREFIX}/bin/pip install opencv-python pycocotools
+${CONDA_PREFIX}/bin/pip install \
+    opencv-python pycocotools pillow-avif-plugin
 
 ### Install PyTorch, ONNX Runtime ###
 
@@ -26,13 +27,18 @@ if [ ${OS_NAME} == "Darwin" ]; then
         pytorch=2.1.2 torchvision=0.16.2 
 elif [ ${OS_NAME} == "Linux" ]; then
     echo "Installing PyTorch, ONNX Runtime for Linux..."
-    echo "To be implemented."
-    conda install -y -p ${CONDA_PREFIX} onnxruntime-gpu
+    ${CONDA_PREFIX}/bin/pip install onnxruntime-gpu
+    conda install -y -p ${CONDA_PREFIX} -c pytorch -c nvidia \
+        pytorch=2.1.2 torchvision=0.16.2 pytorch-cuda=11.8
 else
-    echo "Unsupported operating system: ${OS_NAME}"
+    echo "Unsupported operating system: ${OS_NAME}."
 fi
 
 ### Install SAM ###
 
 ${CONDA_PREFIX}/bin/pip install -e .
+
+### Set up the kernel for Jupyter ###
+
+${CONDA_PREFIX}/bin/ipython kernel install --user --name=sam
 
